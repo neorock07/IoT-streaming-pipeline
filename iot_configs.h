c97f1e9ef10f614089f5d36541bde1ce15fa0a08
Eka Yulianto <99853004+neorock07@users.noreply.github.com>
2025-08-19 19:53:19 +0700

    add: code for esp

---
+++ iot_configs.h
@@ -0,0 +1,12 @@
+
+// Wifi
+#define IOT_CONFIG_WIFI_SSID "Wisma UI 2"
+#define IOT_CONFIG_WIFI_PASSWORD "HEFAZA123"
+
+// Azure IoT
+#define IOT_CONFIG_IOTHUB_FQDN "esp-dht.azure-devices.net"
+#define IOT_CONFIG_DEVICE_ID "esp8266-bento"
+#define IOT_CONFIG_DEVICE_KEY "---KODE--"
+
+// Publish 1 message every 10 seconds
+#define TELEMETRY_FREQUENCY_MILLISECS 1000
